73f8c404dd0c98b07eddb279b013765424ae3019
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-04-27 15:03:08 +0530

    Cognito resource get user get attributes

--- Cognito-Resource/lambda.js
+++ Cognito-Resource/lambda.js
@@ -2,12 +2,14 @@ let AWS = require('aws-sdk');
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 exports.handler = function (event, context, callback) {
 	cognito_idp.listUsers({
-		UserPoolId: "us-east-1_7IHKjs5eP"
+		UserPoolId: "us-east-1_7IHKjs5eP",
+		AttributesToGet: '[email]',
+		PaginationToken: '1'
 	}, function (error, data) {
 		if (error) {
 			// implement error handling logic here
 			throw error;
-		}else{
+		} else {
 			console.log(data);
 		}
 		// your logic goes within this block
