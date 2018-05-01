eb3ccd75a0a95f6d8848a22f77295e7857ac0092
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-05-01 09:56:00 +0530

    admin get user

--- Cognito-Resource/lambda.js
+++ Cognito-Resource/lambda.js
@@ -14,35 +14,7 @@ exports.handler = function (event, context, callback) {
 		}
 		// your logic goes within this block
 	});
-	cognito_idp.adminCreateUser({
-		UserPoolId: "us-east-1_7IHKjs5eP",
 
-		/* required */
-		Username: "andun",
-
-		/* required */
-		DesiredDeliveryMediums: ["EMAIL"],
-		//[SMS | EMAIL,/* more items */],
-		ForceAliasCreation: false,
-		MessageAction: "SUPPRESS",
-		TemporaryPassword: "Andun!12345",
-		UserAttributes: [{
-			Name: 'email',
-			Value: 'andun@adroitlogic.com'
-		}, {
-			Name: 'phone_number',
-			Value: '+94770630943'
-		}],
-		ValidationData: []
-	}, function (error, data) {
-		if (error) {
-			// implement error handling logic here
-			throw error;
-		}
-		console.log("Admin create a user");
-		console.log(data);
-		// your logic goes within this block
-	});
 
 	cognito_idp.adminGetUser({
 		UserPoolId: "us-east-1_7IHKjs5eP", /* required */
